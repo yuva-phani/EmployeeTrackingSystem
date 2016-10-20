91ba87c732fcd464e794d2e5d93c273ed55d59ca
yuva <yuva.phani@gmail.com>
2016-10-21 00:10:21 +0530

    Refactoring

--- src/main/java/com/ilimi/employeetracker/utils/DateTimeUtils.scala
+++ src/main/java/com/ilimi/employeetracker/utils/DateTimeUtils.scala
@@ -7,83 +7,72 @@ import org.joda.time.DateTime
 import org.joda.time.Months
 import org.joda.time.Weeks
 import org.joda.time.format.DateTimeFormat
-import com.ilimi.employeetracker.main.Properties
-
-
+import com.ilimi.employeetracker.utils.PropertyReader
 
 object DateTimeUtils {
 
+  def strDateTotimeInSeconds(x: String): Long = {
+    DateTimeFormat.forPattern(PropertyReader.getProperty("onlyDayFormat") + " " + PropertyReader.getProperty("onlyTimeFormat")).parseDateTime(x).getMillis() / 1000
+  }
 
+  def strDateToDay(x: String): String = {
+    DateTimeFormat.forPattern(PropertyReader.getProperty("onlyDayFormat") + " " + PropertyReader.getProperty("onlyTimeFormat")).parseDateTime(x).toYearMonthDay().toString()
+  }
 
-	def strDateTotimeInSeconds(x: String): Long = {
-			DateTimeFormat.forPattern(Properties.onlyDayFormat+" "+Properties.onlyTimeFormat).parseDateTime(x).getMillis() / 1000
-	}
+  def strToOnlyTimeInSeconds(x: String): Long = {
+    val parseFormat = new SimpleDateFormat(PropertyReader.getProperty("onlyDayFormat") + " " + PropertyReader.getProperty("onlyTimeFormat"))
+    val printFormat = new SimpleDateFormat(PropertyReader.getProperty("onlyTimeFormat"))
+    val date = parseFormat.parse(x)
+    val time = printFormat.format(date).toString()
+    val tokens = time.split(":")
+    val hours = Integer.parseInt(tokens(0))
+    val minutes = Integer.parseInt(tokens(1))
+    val seconds = Integer.parseInt(tokens(2))
+    val duration = 3600 * hours + 60 * minutes + seconds
+    return duration
+  }
 
+  def calculateWeekDays(fromDate: String, toDate: String): Int = {
 
-	def strDateToDay(x: String): String = {
-			DateTimeFormat.forPattern(Properties.onlyDayFormat+" "+Properties.onlyTimeFormat).parseDateTime(x).toYearMonthDay().toString()
-	}
+    val df = new SimpleDateFormat(PropertyReader.getProperty("onlyDayFormat"))
+    val date1 = df.parse(fromDate)
+    val date2 = df.parse(toDate)
+    val cal1 = Calendar.getInstance()
+    val cal2 = Calendar.getInstance()
+    cal1.setTime(date1)
+    cal2.setTime(date2)
 
+    var numberOfDays = 1;
+    while (cal1.before(cal2)) {
+      if ((Calendar.SATURDAY != cal1.get(Calendar.DAY_OF_WEEK))
+        && (Calendar.SUNDAY != cal1.get(Calendar.DAY_OF_WEEK))) {
+        numberOfDays += 1;
+      }
+      cal1.add(Calendar.DATE, 1)
+    }
+    return numberOfDays
+  }
 
-	def strToOnlyTimeInSeconds(x: String): Long = {
-			val parseFormat = new SimpleDateFormat(Properties.onlyDayFormat+" "+Properties.onlyTimeFormat)
-			val printFormat = new SimpleDateFormat(Properties.onlyTimeFormat)
-			val date = parseFormat.parse(x)
-			val time = printFormat.format(date).toString()
-			val tokens = time.split(":")
-			val hours = Integer.parseInt(tokens(0))
-			val minutes = Integer.parseInt(tokens(1))
-			val seconds = Integer.parseInt(tokens(2))
-			val duration = 3600 * hours + 60 * minutes + seconds;
-			return duration
-	}
+  //no of weeks between two dates
 
+  def noOfWeeksBetweenTwoDates(fromDate: String, toDate: String): Int = {
+    val dateTime1 = new DateTime(fromDate);
+    val dateTime2 = new DateTime(toDate);
+    val weeks = Weeks.weeksBetween(dateTime1, dateTime2).getWeeks();
+    return weeks
+  }
 
-	def calculateWeekDays(fromDate: String, toDate: String): Int = {
+  def noOfMonthsBetweenTwoDates(fromDate: String, toDate: String): Int = {
+    val dateTime1 = new DateTime(fromDate);
+    val dateTime2 = new DateTime(toDate);
+    return Months.monthsBetween(dateTime1, dateTime2).getMonths();
+  }
 
-			val df = new SimpleDateFormat(Properties.onlyDayFormat)
-			val  date1 = df.parse(fromDate)
-			val date2 = df.parse(toDate)
-			val cal1 = Calendar.getInstance()
-			val cal2 = Calendar.getInstance()
-			cal1.setTime(date1)
-			cal2.setTime(date2)
-
-			var numberOfDays = 1;
-			while (cal1.before(cal2)) {
-				if ((Calendar.SATURDAY != cal1.get(Calendar.DAY_OF_WEEK))
-						&&(Calendar.SUNDAY != cal1.get(Calendar.DAY_OF_WEEK))) {
-					numberOfDays+=1;
-				}
-				cal1.add(Calendar.DATE,1)
-			}
-			return numberOfDays
-	}
-
-	//no of weeks between two dates
-
-	def noOfWeeksBetweenTwoDates(fromDate: String, toDate: String): Int = {
-			val dateTime1 = new DateTime(fromDate);
-			val dateTime2 = new DateTime(toDate);
-			val weeks = Weeks.weeksBetween(dateTime1, dateTime2).getWeeks();
-			return weeks
-	}
-
-
-	def noOfMonthsBetweenTwoDates(fromDate: String, toDate: String): Int = {
-			val dateTime1 = new DateTime(fromDate);
-			val dateTime2 = new DateTime(toDate);
-			return Months.monthsBetween(dateTime1, dateTime2).getMonths();
-	}
-
-	def epocTimeToDate(x: Long): String = {
-			val formatter = new SimpleDateFormat(Properties.onlyDayFormat.toString());
-			val calendar = Calendar.getInstance();
-			calendar.setTimeInMillis(x * 1000);
-			return formatter.format(calendar.getTime()).toString()
-	}
-
-
-
+  def epocTimeToDate(x: Long): String = {
+    val formatter = new SimpleDateFormat(PropertyReader.getProperty("onlyDayFormat").toString());
+    val calendar = Calendar.getInstance();
+    calendar.setTimeInMillis(x * 1000);
+    return formatter.format(calendar.getTime()).toString()
+  }
 
 }
--- src/test/java/com/ilimi/employeetracker/test/TestEmployeeTrackingService.scala
+++ src/test/java/com/ilimi/employeetracker/test/TestEmployeeTrackingService.scala
@@ -5,37 +5,37 @@ import org.apache.spark.rdd._
 import org.scalatest.FlatSpec
 import com.datastax.spark.connector._
 import com.ilimi.employeetracker.service.EmployeeTrackingService
+import com.ilimi.employeetracker.service.EmployeeTrackingService
 
 class TestEmployeeTrackingService extends FlatSpec {
 
-	val test=EmployeeTrackingService.broadCastStartDate
+  val test = EmployeeTrackingService.broadCastStartDate
 
-			val sc=EmployeeTrackingService.sc
-			val rdd = sc.cassandraTable("excelsior", "word")
-			val cassandratableName = rdd.tableName
-			val firstEmployeeId=rdd.first().get[String]("empid")
-			val predectedfirstlogin=rdd.first().get[String]("predectedfirstlogin")
+  val sc = EmployeeTrackingService.sc
+  val rdd = sc.cassandraTable("excelsior", "word")
+  val cassandratableName = rdd.tableName
+  val firstEmployeeId = rdd.first().get[String]("empid")
+  val predectedfirstlogin = rdd.first().get[String]("predectedfirstlogin")
 
-			val totalRowCount=rdd.count()
+  val totalRowCount = rdd.count()
 
-			"TimeInOffice" should "pop values" in {
+  "TimeInOffice" should "pop values" in {
 
-		assert(firstEmployeeId=="7")
+    assert(firstEmployeeId == "7")
 
-	}
+  }
 
-	it should "pop row count" in {
-		assert(totalRowCount==23)
+  it should "pop row count" in {
+    assert(totalRowCount == 23)
 
-	}
+  }
 
-	it should "pop predectedfirstlogin  "in {
-		assert(predectedfirstlogin=="22420.21319051704,29943.78680948296")
+  it should "pop predectedfirstlogin  " in {
+    assert(predectedfirstlogin == "22420.21319051704,29943.78680948296")
 
-	}
-	it should "give cassandra Table name" in {
+  }
+  it should "give cassandra Table name" in {
 
-
-		assert(cassandratableName=="word")
-	}
+    assert(cassandratableName == "word")
+  }
 }
